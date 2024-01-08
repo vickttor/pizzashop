16e07b1711dde77d46a218b51b226450c5e00133
Victor Silva <victor.silvapc@hotmail.com>
2024-01-08 19:44:31 -0300

    feat: :tada: initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from '@/pages/_layouts/app'
+import { Dashboard } from '@/pages/app/dashboard'
+import { SignIn } from '@/pages/auth/signIn'
+
+import { AuthLayout } from './pages/_layouts/auth'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
+])

67319370f5d98bd78079d6e2c8d12659f5eb5535
Victor Silva <victor.silvapc@hotmail.com>
2024-01-14 12:15:03 -0300

    feat: :sparkles: added dialog to open order information

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from '@/pages/_layouts/app'
+import { AuthLayout } from '@/pages/_layouts/auth'
 import { Dashboard } from '@/pages/app/dashboard'
+import { Orders } from '@/pages/app/orders/orders'
 import { SignIn } from '@/pages/auth/signIn'
-
-import { AuthLayout } from './pages/_layouts/auth'
-import { Orders } from './pages/app/orders.tsx/orders'
-import { SignUp } from './pages/auth/signUp'
+import { SignUp } from '@/pages/auth/signUp'
 
 export const router = createBrowserRouter([
   {

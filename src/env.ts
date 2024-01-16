46d9d1127b40932704c2bdf134a243dbb7d3340d
Victor Silva <victor.silvapc@hotmail.com>
2024-01-16 20:21:05 -0300

    feat: applied request to render profile informations and managed restaurant informations

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   VITE_API_URL: z.string().url(),
+  VITE_ENABLE_API_DELAY: z.string().transform((v) => v === 'true'),
 })
 
 // Vite does not bring envs from process.env
